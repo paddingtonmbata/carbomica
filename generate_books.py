1230de7d7cdcf20b404024e05be11e286bf191cd
farahhoudroge <farah.houdroge@outlook.com>
2023-06-06 16:49:35 +1000

    adjust prog years

--- generate_books.py
+++ generate_books.py
@@ -1,5 +1,4 @@
 import atomica as at
-import numpy as np
 from utils import facilities, interventions
 '''
 Script to generate a databook and progbook.
@@ -8,16 +7,15 @@ Script to generate a databook and progbook.
 F = at.ProjectFramework('files/carbomica_framework.xlsx') 
 
 #%% Step 1: generate databook
-data_years = [2022,2023] # years for input data
+data_years = 2023 # years for input data
 databook_name = 'files/carbomica_databook.xlsx' # databook name
 gen_databook = False # 'True' if generating databooks, 'False' if generating progbooks
 
 if gen_databook :
-    D = at.ProjectData.new(F,np.arange(data_years[0],data_years[1]),pops=facilities,transfers=0)
+    D = at.ProjectData.new(F,data_years,pops=facilities,transfers=0)
     D.save(databook_name)
 
 #%% Step 2: generate program books after filling in databook (note that progbook will not generate if data cells in databook are empty)
-prog_years = [2024,2030] # program years
 nb_prog = 8 # number of programs
 gen_progbook = True # 'True' if generating progbooks, 'False' if generating databooks
 
@@ -25,4 +23,4 @@ if gen_progbook :
     P = at.Project(framework=F,databook=databook_name, do_run=False)
     for facility in facilities.keys():
         progbook_path = 'files/carbomica_progbook_{}.xlsx'.format(facility)
-        P.make_progbook(progbook_path,progs=interventions,data_start=prog_years[0],data_end=prog_years[1])
+        P.make_progbook(progbook_path,progs=interventions,data_start=data_years,data_end=data_years)
--- populate_books.py
+++ populate_books.py
@@ -1,17 +1,15 @@
 import atomica as at
 import pandas as pd
-import numpy as np
 from utils import facilities, interventions
 import os
 if not os.path.exists('books'): os.makedirs('books')
 
 '''
 The script to create fresh databooks and progbooks. Will overwrite existing 
-files in Databooks and Progbooks folders.
+files in books folders.
 '''
 
-data_years = [2023]
-prog_years = np.arange(2024,2031)
+data_years = 2023
 
 F = at.ProjectFramework('files/carbomica_framework.xlsx')
 D = at.ProjectData.new(framework=F, tvec=data_years, pops=facilities, transfers=0)
@@ -26,7 +24,7 @@ for facility in facilities.keys():
     P = at.ProgramSet.from_spreadsheet(spreadsheet='files/carbomica_progbook_{}.xlsx'.format(facility), framework=F, data=D, _allow_missing_data=True)
     for intervention in interventions.keys():
         P.programs[intervention].unit_cost = at.TimeSeries(assumption=pb_costs.loc[facility,intervention], units='$/person/year')
-        P.programs[intervention].spend_data = at.TimeSeries(prog_years[0],1e-16, units='$/year') # make it a small, negligible but non-zero number for optimisation
+        P.programs[intervention].spend_data = at.TimeSeries(data_years,1e-16, units='$/year') # make it a small, negligible but non-zero number for optimisation
         P.programs[intervention].capacity_constraint = at.TimeSeries(assumption=1, units='people')
         P.programs[intervention].coverage = at.TimeSeries(assumption=1, units='people/year')
     P.save('books/carbomica_progbook_{}.xlsx'.format(facility))    
